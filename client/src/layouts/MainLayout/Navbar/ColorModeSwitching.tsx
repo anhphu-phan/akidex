4b487f93b8925d7f3144dfa596b27ccb6ab8bd2a
anhphuphan <anhphuphan.cs@gmail.com>
2022-08-23 20:02:41 +0700

    [client] fixed navbar toggle mode button

--- client/src/layouts/MainLayout/Navbar/ColorModeSwitching.tsx
+++ client/src/layouts/MainLayout/Navbar/ColorModeSwitching.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { IconButton, SxProps, Theme } from "@mui/material"
 import LightModeIcon from "@mui/icons-material/LightMode"
-import BedtimeIcon from '@mui/icons-material/Bedtime';
+import BedtimeIcon from "@mui/icons-material/Bedtime"
 import { useAppDispatch, useAppSelector } from "store/hooks"
 import { selectColorScheme, toggleMode } from "store/slices/customizationSlice"
 
@@ -18,8 +18,18 @@ const ColorModeSwitching = ({ sx }: ColorModeSwitchingProps) => {
     }
 
     return (
-        <IconButton sx={{ p: 0.5, color: "#fff", border: "1px solid #E0E3E7", ...sx }} onClick={handleToggleColorMode}>
-            {colorMode === "light" ? <BedtimeIcon /> : <LightModeIcon color='warning' />}
+        <IconButton
+            sx={{
+                p: 0.5,
+                color: "#fff",
+                border: "1px solid #E0E3E7",
+                width: 40,
+                height: 40,
+                ...sx,
+            }}
+            onClick={handleToggleColorMode}
+        >
+            {colorMode === "light" ? <BedtimeIcon /> : <LightModeIcon color="warning" />}
         </IconButton>
     )
 }
