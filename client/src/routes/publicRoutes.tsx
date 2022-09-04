e56c1a61d531014c41cd0bc7874f1dde7fb2c041
anhphuphan <anhphuphan.cs@gmail.com>
2022-09-04 16:01:39 +0700

    [client] update public routes

--- client/src/routes/publicRoutes.tsx
+++ client/src/routes/publicRoutes.tsx
@@ -2,11 +2,12 @@ import React from "react"
 import { RouteObject } from "react-router-dom"
 import { MainLayout } from "layouts"
 import { HomePage } from "views/common"
-import { AnimeHomePage, TopAnime, AnimeSearch, AnimeSearchResult } from "views/anime"
-import { MangaHomePage, TopManga, MangaSearch, MangaSearchResult } from "views/manga"
+import { AnimeHomePage, TopAnime, AnimeSearch } from "views/anime"
+import { MangaHomePage, TopManga, MangaSearch, MangaDetail } from "views/manga"
 import { VisualNovelHomePage, TopVisualNovel, VisualNovelSearch, VisualNovelSearchResult } from "views/visualNovel"
 import SeasonalAnime from "views/anime/SeasonalAnime"
 import AnimeDetail from "views/anime/AnimeDetail"
+import { MediaOverview, MediaCharacters, MediaStaff, MediaRelations } from "layouts/MediaDetail"
 
 const publicRoutes: RouteObject = {
     path: "/",
@@ -24,18 +25,35 @@ const publicRoutes: RouteObject = {
                     element: <MangaHomePage />,
                 },
                 {
+                    path: ":id",
+                    element: <MangaDetail />,
+                    children: [
+                        {
+                            index: true,
+                            path: "",
+                            element: <MediaOverview />,
+                        },
+                        {
+                            path: "characters",
+                            element: <MediaCharacters />,
+                        },
+                        {
+                            path: "staff",
+                            element: <MediaStaff />,
+                        },
+                        {
+                            path: "relations",
+                            element: <MediaRelations />,
+                        },
+                    ],
+                },
+                {
                     path: "ranking",
                     element: <TopManga />,
                 },
                 {
                     path: "search",
                     element: <MangaSearch />,
-                    children: [
-                        {
-                            path: "result",
-                            element: <MangaSearchResult />,
-                        },
-                    ],
                 },
             ],
         },
@@ -49,6 +67,25 @@ const publicRoutes: RouteObject = {
                 {
                     path: ":id",
                     element: <AnimeDetail />,
+                    children: [
+                        {
+                            index: true,
+                            path: "",
+                            element: <MediaOverview />,
+                        },
+                        {
+                            path: "characters",
+                            element: <MediaCharacters />,
+                        },
+                        {
+                            path: "staff",
+                            element: <MediaStaff />,
+                        },
+                        {
+                            path: "relations",
+                            element: <MediaRelations />,
+                        },
+                    ],
                 },
                 {
                     path: "ranking",
@@ -57,12 +94,6 @@ const publicRoutes: RouteObject = {
                 {
                     path: "search",
                     element: <AnimeSearch />,
-                    children: [
-                        {
-                            path: "result",
-                            element: <AnimeSearchResult />,
-                        },
-                    ],
                 },
                 {
                     path: "season",
