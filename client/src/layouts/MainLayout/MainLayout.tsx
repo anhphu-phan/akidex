75691ea7349c6813b4b5c288772e36e5f5b2d874
anhphuphan <anhphuphan.cs@gmail.com>
2022-09-04 16:01:58 +0700

    [client] update Main layout

--- client/src/layouts/MainLayout/MainLayout.tsx
+++ client/src/layouts/MainLayout/MainLayout.tsx
@@ -1,17 +1,14 @@
-import { Box, Container, Unstable_Grid2 as Grid } from "@mui/material"
 import React from "react"
+import { Box } from "@mui/material"
 import { Outlet } from "react-router-dom"
-import { LeftSidebar } from "./LeftSidebar"
 import { Navbar } from "./Navbar"
-import { RightSidebar } from "./RightSidebar"
 
 const MainLayout = () => {
     return (
         <Box>
             <Navbar />
-            <Container maxWidth="xl" disableGutters>
                 <Outlet />
-            </Container>
+
         </Box>
     )
 }
